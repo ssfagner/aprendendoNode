3fcae04502f9523222c10c0ac2e80715f25f9124
Fagner Santos <fagner.0001@outlook.com>
2022-02-01 18:30:32 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Carregando modulos
+const express = require('express');
+const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
+const app = express();
+const admin = require('./routes/admin');
+//const mongoose = require('mongoose')
+
+//Configurações
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//Handlebars
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+// Mongoose
+// Em Breve
+// Rotas
+app.use('/admin', admin);
+// Outros
+const PORT = 8081;
+app.listen(PORT, () => {
+  console.log('Servidor Rodando!');
+});
