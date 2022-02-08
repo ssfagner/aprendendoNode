3d0e329450d3d7737292fa532392f3ccf918999b
Fagner Santos <fagner.0001@outlook.com>
2022-02-08 20:42:31 -0300

    desenvolvimento

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,87 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const mongoose = require('mongoose');
+const router = express.Router();
+require('../models/Categoria');
+const Categoria = mongoose.model('categorias');
 
 router.get('/', (req, res) => {
-  res.render('admin/index')
-})
+  res.render('admin/index');
+});
 
 router.get('/posts', (req, res) => {
-  res.send('Pagina de posts')
-})
+  res.send('Pagina de posts');
+});
 
 router.get('/categorias', (req, res) => {
-  res.send('Pagina de categorias')
-})
+  Categoria.find()
+    .sort({ date: 'desc' })
+    .lean()
+    .then((categorias) => {
+      res.render('admin/categorias', { categorias: categorias });
+    })
+    .catch((err) => {
+      req.flash('error_msg', 'Houve um erro ao listar as categorias');
+      res.redirect('/admin');
+    });
+});
+
+router.get('/categorias/add', (req, res) => {
+  res.render('admin/addcategorias');
+});
+
+router.post('/categorias/nova', (req, res) => {
+  var erros = [];
+
+  if (
+    !req.body.nome ||
+    typeof req.body.nome == undefined ||
+    req.body.nome == null
+  ) {
+    erros.push({ texto: 'Nome inválido' });
+  }
+
+  if (
+    !req.body.slug ||
+    typeof req.body.slug == undefined ||
+    req.body.slug == null
+  ) {
+    erros.push({ texto: 'Slug inválido' });
+  }
+
+  if (req.body.nome.length < 2) {
+    erros.push({ texto: 'Nome da categoria é muito pequeno' });
+  }
+
+  if (erros.length > 0) {
+    res.render('admin/addcategorias', { erros: erros });
+  } else {
+    const novaCategoria = {
+      nome: req.body.nome,
+      slug: req.body.slug,
+    };
+
+    new Categoria(novaCategoria)
+      .save()
+      .then(() => {
+        req.flash('success_msg', 'Categoria criada com sucesso');
+        res.redirect('/admin/categorias');
+      })
+      .catch((err) => {
+        req.flash(
+          'error_msg',
+          'Houve um erro ao salvar a categoria, tente novamente!',
+        );
+        res.redirect('/admin');
+
+        console.log('Erro ao salvar categoria');
+      });
+  }
+});
 
 router.get('/teste', (req, res) => {
-  res.send('Pagina de testes')
-})
+  res.send('Pagina de testes');
+});
 
-module.exports = router
+module.exports = router;
