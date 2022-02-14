8e2a7e47f521bc203b4fb327f5d20ce82b529b53
Fagner Santos <fagner.0001@outlook.com>
2022-02-14 20:18:25 -0300

    criando formulário de postagens

--- routes/admin.js
+++ routes/admin.js
@@ -130,8 +130,20 @@ router.post('/categorias/deletar', (req, res) => {
     });
 });
 
-router.get('/teste', (req, res) => {
-  res.send('Pagina de testes');
+router.get('/postagens', (req, res) => {
+  res.render('admin/postagens');
+});
+
+router.get('/postagens/add', (req, res) => {
+  Categoria.find()
+    .lean()
+    .then((categorias) => {
+      res.render('admin/addpostagem', { categorias: categorias });
+    })
+    .catch((err) => {
+      req.flash('error_msg', 'Houve um erro ao carregar o formulário');
+      res.redirect('/admin');
+    });
 });
 
 module.exports = router;
