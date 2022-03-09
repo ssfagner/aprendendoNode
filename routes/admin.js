90558b948aeff5d758ddf4a1c88186920e6fbc27
Fagner Santos <fagner.0001@outlook.com>
2022-03-09 20:00:56 -0300

    deletando postagens

--- routes/admin.js
+++ routes/admin.js
@@ -246,4 +246,15 @@ router.post('/postagens/edit', (req, res) => {
     });
 });
 
+router.get('/postagens/deletar/:id', (req, res) => {
+  Postagem.remove({ _id: req.params.id })
+    .then(() => {
+      req.flash('success_msg', 'Postagem deletada com sucesso!');
+      res.redirect('/admin/postagens');
+    })
+    .catch((err) => {
+      req.flash('error_msg', 'Houve um erro ao deletar a postagem!');
+    });
+});
+
 module.exports = router;
