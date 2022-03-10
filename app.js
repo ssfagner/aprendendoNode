2d1863bd723948900f3ff802538ccea9679aba00
Fagner Santos <fagner.0001@outlook.com>
2022-03-09 21:03:40 -0300

    home da pagina

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('connect-flash');
+require('./models/Postagem');
+const Postagem = mongoose.model('postagem');
 
 //Configurações
 //Sessão
@@ -45,6 +47,28 @@ mongoose
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas
+app.get('/', (req, res) => {
+  Postagem.find()
+
+    .populate('categoria')
+    .sort({ data: 'desc' })
+    .lean()
+    .then((postagens) => {
+      res.render('index', { postagens: postagens });
+    })
+    .catch((err) => {
+      req.flash('error_msg', 'Houve um erro ao carregar as postagens');
+      res.redirect('/404');
+    });
+});
+
+app.get('/404', (req, res) => {
+  res.send('Erro 404!');
+});
+
+app.get('/posts', (req, res) => {
+  res.send('Lista Posts');
+});
 app.use('/admin', admin);
 // Outros
 const PORT = 8081;
